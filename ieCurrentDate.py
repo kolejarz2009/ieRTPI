0e40840ae2f36f1beef1cd7598f33dab9c037d95
kolejarz2009 <kolejarz2009@users.noreply.github.com>
2018-01-20 02:40:23 +0000

    Now ieCurrentDate works!!

--- ieCurrentDate.py
+++ ieCurrentDate.py
@@ -1,45 +1,37 @@
+# This script establishes the current date, in the format used by Irish Rail, in their public API
 import datetime
-now=datetime.datetime.now()
+CurrentMonth=datetime.datetime.now().month
 
-CurrentYear=print(now.year)
-CurrentMonth=print(now.month)
-CurrentDay=print(now.day)
-CurrentHour=print(now.hour)
-CurrentMinute=print(now.minute)
-
-if True==True:
-#def GetCurrentDate("Yes")
-    Year=CurrentYear
-    if CurrentMonth=="12":
+Year=datetime.datetime.now().year
+if (CurrentMonth)==12:
         Month="dec"
-    if CurrentMonth=="11":
+if CurrentMonth==11:
         Month="nov"
-    if CurrentMonth=="10":
-	Month="oct"
-    if CurrentMonth=="9":
-	Month="sep"
-    if CurrentMonth=="8":
-	Month="aug"
-    if CurrentMonth=="7":
-	Month="jul"
-    if CurrentMonth=="6":
-	Month="jun"
-    if CurrentMonth=="5":
-	Month="may"
-    if CurrentMonth=="4":
-	Month="apr"
-    if CurrentMonth=="3":
-	Month="mar"
-    if CurrentMonth=="2":
-	Month="feb"
-    if CurrentMonth=="1":
-	Month="jan"
-    else:
-	print("Error in retriving current date!")
-    Day=CurrentDay
+if CurrentMonth==10:
+        Month="oct"
+if CurrentMonth==9:
+        Month="sep"
+if CurrentMonth==8:
+        Month="aug"
+if CurrentMonth==7:
+        Month="jul"
+if CurrentMonth==6:
+        Month="jun"
+if CurrentMonth==5:
+        Month="may"
+if CurrentMonth==4:
+        Month="apr"
+if CurrentMonth==3:
+        Month="mar"
+if CurrentMonth==2:
+        Month="feb"
+if CurrentMonth==1:
+        Month="Jan"
+Day=datetime.datetime.now().day
 
-IEDateFormat=Day+"%20 "+Month+"%20 "+Year
-print(IEDateFormat)
+IEDateFormat = "%s %s %s" % (Day, Month, Year)
+global Date
+Date = IEDateFormat
 
-#http://api.irishrail.ie/realtime/realtime.asmx/getTrainMovementsXML?TrainId=e109&TrainDate=21%20dec%202011
 
+print(Date)
