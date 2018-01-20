0dc8382d46e0723a908d39c36af2ea25eb834bb3
Marek <marek.starzec@mydit.ie>
2018-01-20 02:55:36 +0000

    Shit changes

--- GetWebsiteAddress.py
+++ GetWebsiteAddress.py
@@ -1,2 +1,4 @@
-Website="http://api.irishrail.ie/realtime/realtime.asmx/getTrainMovementsXML?TrainId="Headcode,"&TrainDate="IEDateFormat
-
+from ieCurrentDate import Date
+Headcode=input("Headcode: ")
+Website="http://api.irishrail.ie/realtime/realtime.asmx/getTrainMovementsXML?TrainId=" + Headcode + "&TrainDate=" + Date
+print(Website)
--- TrainRealtimeWebsiteParser.py
+++
@@ -1,6 +0,0 @@
-Website="http://api.irishrail.ie/realtime/realtime.asmx/getTrainMovementsXML?TrainId=a125&TrainDate=28%20apr%202017"
-
-'''
-<objTrainMovements><TrainCode>A125 </TrainCode><TrainDate>28 Apr 2017</TrainDate><LocationCode>MHIDE</LocationCode><LocationFullName>Malahide</LocationFullName><LocationOrder>12</LocationOrder><LocationType>T</LocationType><TrainOrigin>Belfast Central</TrainOrigin><TrainDestination>Dublin Connolly</TrainDestination><ScheduledArrival>12:28:00</ScheduledArrival><ScheduledDeparture>12:28:00</ScheduledDeparture><ExpectedArrival>12:35:24</ExpectedArrival><ExpectedDeparture>12:35:18</ExpectedDeparture><Arrival>12:35:18</Arrival><Departure>12:35:30</Departure><AutoArrival>1</AutoArrival><AutoDepart>1</AutoDepart><StopType>-</StopType></objTrainMovements>
-'''
-
